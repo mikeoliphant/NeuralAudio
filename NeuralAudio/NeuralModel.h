4c5b7d3e1f73266bdcece37537458e18929fedbc
Mike Oliphant <oliphant@nostatic.org>
2024-10-30 18:53:01 -0700

    Basic NAM core implementation. Use RTNeural's copy of json

--- NeuralAudio/NeuralModel.h
+++ NeuralAudio/NeuralModel.h
@@ -1,2 +1,37 @@
 #pragma once
 
+#include <filesystem>
+#include <NAM/dsp.h>
+
+namespace NeuralAudio
+{
+	class NeuralModel
+	{
+	public:
+		static NeuralModel* CreateFromFile(std::filesystem::path modelPath)
+		{
+			NeuralModel model = NeuralModel();
+			model.namModel = nam::get_dsp(modelPath);
+		}
+
+		float GetRecommendedOutputDBAdjustment()
+		{
+			if (namModel->HasLoudness())
+			{
+				return namModel->GetLoudness();
+			}
+			else
+			{
+				return 1.0f;
+			}
+		}
+
+		void Process(std::vector<float> input, std::vector<float> output)
+		{
+			namModel->process(input.data(), output.data(), input.size());
+		}
+
+	private:
+		std::unique_ptr<nam::DSP> namModel;
+	};
+}
