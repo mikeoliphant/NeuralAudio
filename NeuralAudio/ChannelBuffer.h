d6592670124a6b4ca8cbdd247ea6cdc7d7459a65
Mike Oliphant <oliphant@nostatic.org>
2026-07-15 09:05:11 -0700

    Add ChannelBuffer classes (#62)

---
+++ NeuralAudio/ChannelBuffer.h
@@ -0,0 +1,219 @@
+#pragma once
+
+#include <array>
+#include <Eigen/Dense>
+#include <Eigen/Core>
+
+namespace NeuralAudio
+{
+	template<typename T, int Channels>
+	class ChannelRowSpan;
+
+	template<typename T, int Channels>
+	class ChannelBufferBase
+	{
+		public:
+			virtual size_t GetNumCols() const { return 0; }
+			size_t GetNumChannels() const { return Channels; }
+			virtual T* GetData() { return nullptr; }
+			virtual T* GetData(size_t startCol)
+			{
+				(void)startCol;
+
+				return nullptr;
+			}
+			virtual const T* GetDataConst() const { return nullptr; }
+			virtual const T* GetDataConst(size_t startCol) const
+			{
+				(void)startCol;
+
+				return nullptr;
+			}
+			virtual void SetZero() {}
+	};
+
+	template<typename T, int Channels, int Cols>
+	class ChannelBuffer : public ChannelBufferBase<T, Channels>
+	{
+		public:
+			ChannelBuffer()
+			{
+			}
+			
+			size_t GetNumCols() const override
+			{
+				return Cols;
+			}
+
+			T* GetData() override
+			{
+				return data[0].data();
+			}
+
+			const T* GetDataConst() const override
+			{
+				return data[0].data();
+			}
+
+			T* GetData(size_t startCol) override
+			{
+				return data[startCol].data();
+			}
+
+			const T* GetDataConst(size_t startCol) const override
+			{
+				return data[startCol].data();
+			}
+
+			void SetZero() override
+			{
+				for (auto& col : data) {
+					col.fill(0);
+				}
+			}
+
+			T& operator()(size_t row, size_t col)
+			{
+				return data[col][row];
+			}
+
+			const T& operator()(size_t row, size_t col) const
+			{
+				return data[col][row];
+			}
+
+			const ChannelRowSpan<T, Channels> Slice(size_t startCol, size_t numCols)
+			{
+				return ChannelRowSpan<T, Channels>(this, startCol, numCols);
+			}
+
+			const ChannelRowSpan<T, Channels> Slice(size_t numCols)
+			{					
+				return ChannelRowSpan<T, Channels>(this, numCols);
+			}
+
+			const Eigen::Map<Eigen::Matrix<float, Channels, Cols>> GetEigenMap()
+			{
+				return Eigen::Map<Eigen::Matrix<float, Channels, Cols>>(GetData(), Channels, GetNumCols());
+			}
+
+			const Eigen::Map<const Eigen::Matrix<float, Channels, Cols>> GetEigenMapConst() const
+			{
+				return Eigen::Map<const Eigen::Matrix<float, Channels, Cols>>(GetDataConst(), Channels, GetNumCols());
+			}
+
+		private:
+			alignas(32) std::array<std::array<T, Channels>, Cols> data;
+	};
+
+	template<typename T, int Channels>
+	class ChannelRowSpan
+	{
+		public:
+			ChannelRowSpan(ChannelBufferBase<T, Channels>& baseBuffer) :
+				buffer(&baseBuffer),
+				startCol(0),
+				numCols(baseBuffer.GetNumCols())
+			{
+				
+			}
+
+			ChannelRowSpan(ChannelBufferBase<T, Channels>* baseBuffer) :
+				buffer(baseBuffer),
+				startCol(0),
+				numCols(baseBuffer->GetNumCols())
+			{
+			
+			}
+
+			ChannelRowSpan(ChannelBufferBase<T, Channels>* baseBuffer, size_t startCol, size_t numCols) :
+				buffer(baseBuffer),
+				startCol(startCol),
+				numCols(numCols)
+			{
+
+			}
+
+			ChannelRowSpan(ChannelBufferBase<T, Channels>* baseBuffer, size_t numCols) :
+				buffer(baseBuffer),
+				startCol(0),
+				numCols(numCols)
+			{
+
+			}
+
+			const ChannelRowSpan<T, Channels> Slice(size_t startCol, size_t numCols) const
+			{
+				return ChannelRowSpan<T, Channels>(buffer, this->startCol + startCol, numCols);
+			}
+
+			const ChannelRowSpan<T, Channels> Slice(size_t numCols) const
+			{
+				return ChannelRowSpan<T, Channels>(buffer, this->startCol, numCols);
+			}
+
+			size_t GetNumCols() const
+			{
+				return numCols;
+			}
+
+			size_t GetNumChannels() const
+			{
+				return Channels;
+			}
+
+			T* GetData() const
+			{				
+				return buffer->GetData(startCol);
+			}
+
+			const T* GetDataConst() const
+			{
+				return buffer->GetDataConst(startCol);
+			}
+
+			T* GetData(size_t startCol) const
+			{
+				return buffer->GetData(this->startCol + startCol);
+			}
+
+			const T* GetDataConst(size_t startCol) const
+			{
+				return buffer->GetDataConst(this->startCol + startCol);
+			}
+
+			Eigen::Map<Eigen::Matrix<float, Channels, Eigen::Dynamic>> GetEigenMap() const
+			{
+				return Eigen::Map<Eigen::Matrix<float, Channels, Eigen::Dynamic>>(GetData(), Channels, numCols);
+			}
+
+			const Eigen::Map<const Eigen::Matrix<float, Channels, Eigen::Dynamic>> GetEigenMapConst() const
+			{
+				return Eigen::Map<const Eigen::Matrix<float, Channels, Eigen::Dynamic>>(GetDataConst(), Channels, numCols);
+			}
+
+			void CopyData(const ChannelRowSpan<T, Channels>& srcSpan) const
+			{
+				const T* srcPtr = srcSpan.GetDataConst();
+				T* destPtr = GetData();
+
+				memmove(destPtr, srcPtr, Channels * numCols * sizeof(T));
+			}
+
+			void AddData(const ChannelRowSpan<T, Channels>& srcSpan) const
+			{
+				const T* srcPtr = srcSpan.GetDataConst();
+				T* destPtr = GetData();
+
+				for (size_t count = 0; count < Channels * numCols; count++)
+				{
+					*destPtr++ += *srcPtr++;
+				}
+			}
+
+		private:
+			ChannelBufferBase<T, Channels>* const buffer;
+			const size_t startCol;
+			const size_t numCols;
+	};
+}
--- NeuralAudio/MatMul.h
+++ NeuralAudio/MatMul.h
@@ -5,7 +5,9 @@ namespace NeuralAudio
 	template <int InChannels, int OutChannels>
 	struct MatMul
 	{
-		static constexpr bool HasKernel() { return ((InChannels == 3) && ((OutChannels == 3) || (OutChannels == 1))); }
+		#define IsKernel(In, Out) ((InChannels == In) && (OutChannels == Out))
+
+		static constexpr bool HasKernel() { return (IsKernel(3, 3) || IsKernel(8, 1) || IsKernel(3, 1) || IsKernel(1, 3)); }
 
 		// 3x3 implementation inspired by @jfsantos NAM Core a2fast - https://github.com/sdatkinson/NeuralAmpModelerCore/blob/main/NAM/wavenet/a2_fast.cpp
 
@@ -13,7 +15,7 @@ namespace NeuralAudio
 		{
 			static_assert(HasKernel(), "Multiplication not implemented for InChannel/OutChannel combination");
 
-			if constexpr ((InChannels == 3) && (OutChannels == 3))
+			if constexpr (IsKernel(3, 3))
 			{
 				const float w00 = weights[0], w10 = weights[1], w20 = weights[2];
 				const float w01 = weights[3], w11 = weights[4], w21 = weights[5];
@@ -39,7 +41,28 @@ namespace NeuralAudio
 					out[2] = a2;
 				}
 			}
-			else if constexpr ((InChannels == 3) && (OutChannels == 1))
+			else if constexpr (IsKernel(8, 1))
+			{
+				const float w0 = weights[0], w1 = weights[1], w2 = weights[2], w3 = weights[3];
+				const float w4 = weights[4], w5 = weights[5], w6 = weights[6], w7 = weights[7];
+
+				for (size_t frame = 0; frame < numFrames; frame++)
+				{
+					const size_t offset = frame * InChannels;
+					const float* in = &inData[offset];
+
+					float a0 = w0 * in[0];
+					a0 += w1 * in[1];
+					a0 += w2 * in[2];
+					a0 += w3 * in[3];
+					a0 += w4 * in[4];
+					a0 += w5 * in[5];
+					a0 += w6 * in[6];
+					a0 += w7 * in[7];
+					outData[frame] = a0;
+				}
+			}
+			else if constexpr (IsKernel(3, 1))
 			{
 				const float w0 = weights[0], w1 = weights[1], w2 = weights[2];
 
@@ -48,10 +71,26 @@ namespace NeuralAudio
 					const size_t offset = frame * 3;
 					const float* in = &inData[offset];
 
-					float a0 = w0 * in[frame * 3];
+					float a0 = w0 * in[0];
 					a0 += w1 * in[1];
 					a0 += w2 * in[2];
 					outData[frame] = a0;
+				}
+			}
+			else if constexpr (IsKernel(1, 3))
+			{
+				const float w0 = weights[0], w1 = weights[1], w2 = weights[2];
+
+				for (size_t frame = 0; frame < numFrames; frame++)
+				{
+					const size_t offset = frame * 3;
+					float* out = &outData[offset];
+
+					const float in = inData[frame];
+
+					out[0] = w0 * in;
+					out[1] = w1 * in;
+					out[2] = w2 * in;
 				}
 			}
 		}
@@ -60,7 +99,7 @@ namespace NeuralAudio
 		{
 			static_assert(HasKernel(), "Multiplication not implemented for InChannel/OutChannel combination");
 
-			if constexpr ((InChannels == 3) && (OutChannels == 3))
+			if constexpr (IsKernel(3, 3))
 			{
 				const float w00 = weights[0], w10 = weights[1], w20 = weights[2];
 				const float w01 = weights[3], w11 = weights[4], w21 = weights[5];
@@ -86,7 +125,30 @@ namespace NeuralAudio
 					out[2] = a2;
 				}
 			}
-			else if constexpr ((InChannels == 3) && (OutChannels == 1))
+			else if constexpr (IsKernel(8, 1))
+			{
+				const float w0 = weights[0], w1 = weights[1], w2 = weights[2], w3 = weights[3];
+				const float w4 = weights[4], w5 = weights[5], w6 = weights[6], w7 = weights[7];
+
+				const float init = initData[0];
+
+				for (size_t frame = 0; frame < numFrames; frame++)
+				{
+					const size_t offset = frame * InChannels;
+					const float* in = &inData[offset];
+
+					float a0 = init + w0 * in[0];
+					a0 += w1 * in[1];
+					a0 += w2 * in[2];
+					a0 += w3 * in[3];
+					a0 += w4 * in[4];
+					a0 += w5 * in[5];
+					a0 += w6 * in[6];
+					a0 += w7 * in[7];
+					outData[frame] = a0;
+				}
+			}
+			else if constexpr (IsKernel(3, 1))
 			{
 				const float w0 = weights[0], w1 = weights[1], w2 = weights[2];
 
@@ -101,13 +163,30 @@ namespace NeuralAudio
 					outData[frame] = a0;
 				}
 			}
+			else if constexpr (IsKernel(1, 3))
+			{
+				const float w0 = weights[0], w1 = weights[1], w2 = weights[2];
+				const float init0 = inData[0], init1 = inData[1], init2 = inData[2];
+
+				for (size_t frame = 0; frame < numFrames; frame++)
+				{
+					const size_t offset = frame * 3;
+					float* out = &outData[offset];
+
+					const float in = inData[frame];
+
+					out[0] = init0 + (w0 * in);
+					out[1] = init1 + (w1 * in);
+					out[2] = init2 + (w2 * in);
+				}
+			}
 		}
 
 		static inline void MultiplyAccumlulate(const float* inData, float* outData, const float* weights, size_t numFrames)
 		{
 			static_assert(HasKernel(), "Multiplication not implemented for InChannel/OutChannel combination");
 
-			if constexpr ((InChannels == 3) && (OutChannels == 3))
+			if constexpr (IsKernel(3, 3))
 			{
 				const float w00 = weights[0], w10 = weights[1], w20 = weights[2];
 				const float w01 = weights[3], w11 = weights[4], w21 = weights[5];
@@ -133,7 +212,28 @@ namespace NeuralAudio
 					out[2] = a2;
 				}		
 			}
-			else if constexpr ((InChannels == 3) && (OutChannels == 1))
+			else if constexpr (IsKernel(8, 1))
+			{
+				const float w0 = weights[0], w1 = weights[1], w2 = weights[2], w3 = weights[3];
+				const float w4 = weights[4], w5 = weights[5], w6 = weights[6], w7 = weights[7];
+
+				for (size_t frame = 0; frame < numFrames; frame++)
+				{
+					const size_t offset = frame * InChannels;
+					const float* in = &inData[offset];
+
+					float a0 = outData[frame] + w0 * in[0];
+					a0 += w1 * in[1];
+					a0 += w2 * in[2];
+					a0 += w3 * in[3];
+					a0 += w4 * in[4];
+					a0 += w5 * in[5];
+					a0 += w6 * in[6];
+					a0 += w7 * in[7];
+					outData[frame] = a0;
+				}
+			}
+			else if constexpr (IsKernel(3, 1))
 			{
 				const float w0 = weights[0], w1 = weights[1], w2 = weights[2];
 
@@ -146,6 +246,22 @@ namespace NeuralAudio
 					a0 += w1 * in[1];
 					a0 += w2 * in[2];
 					outData[frame] = a0;
+				}
+			}
+			else if constexpr (IsKernel(1, 3))
+			{
+				const float w0 = weights[0], w1 = weights[1], w2 = weights[2];
+
+				for (size_t frame = 0; frame < numFrames; frame++)
+				{
+					const size_t offset = frame * 3;
+					float* out = &outData[offset];
+
+					const float in = inData[frame];
+
+					out[0] += w0 * in;
+					out[1] += w1 * in;
+					out[2] += w2 * in;
 				}
 			}
 		}
