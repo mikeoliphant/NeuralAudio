69b17b6b3a8073e8ff755f71c414f597a44e579e
Mike Oliphant <oliphant@nostatic.org>
2026-07-17 09:04:16 -0700

    Multiframe 8x8 conv 0/4/8. Set default based on arch/compiler

--- NeuralAudio/WaveNet.h
+++ NeuralAudio/WaveNet.h
@@ -126,12 +126,12 @@ namespace NeuralAudio
 			const size_t numFrames = output.GetNumCols();
 			float* __restrict outputPtr = output.GetData();
 
-#if ENABLE_MULTIFRAME_8X8_CONVOLUTION
+#if MULTIFRAME_8X8_CONVOLUTION != 0
 			if constexpr ((InChannels == 8) && (OutChannels == 8) && DoBias)
 			{
 				// Based on @jfsantos NAM Core implementation - https://github.com/sdatkinson/NeuralAmpModelerCore/pull/277
 
-				constexpr size_t tileSize = 8;
+				constexpr size_t tileSize = MULTIFRAME_8X8_CONVOLUTION;
 				const size_t nFTile = (numFrames / tileSize) * tileSize;
 
 				for (size_t f = 0; f < nFTile; f += tileSize)
@@ -148,7 +148,10 @@ namespace NeuralAudio
 						{
 							const float* __restrict Wcol = W + cp * InChannels;
 							const float h0 = hb[cp], h1 = hb[InChannels + cp], h2 = hb[2 * InChannels + cp], h3 = hb[3 * InChannels + cp];
+
+#if MULTIFRAME_8X8_CONVOLUTION == 8
 							const float h4 = hb[4 * InChannels + cp], h5 = hb[5 * InChannels + cp], h6 = hb[6 * InChannels + cp], h7 = hb[7 * InChannels + cp];
+#endif
 
 							for (size_t o = 0; o < InChannels; o++)
 							{
@@ -157,10 +160,12 @@ namespace NeuralAudio
 								a[1][o] += wo * h1;
 								a[2][o] += wo * h2;
 								a[3][o] += wo * h3;
+#if MULTIFRAME_8X8_CONVOLUTION == 8
 								a[4][o] += wo * h4;
 								a[5][o] += wo * h5;
 								a[6][o] += wo * h6;
 								a[7][o] += wo * h7;
+#endif
 							}
 						}
 					}
